9662c775d3ca2479d0c9939b11171703af08448f
Sam Lubin <samlubin@yahoo.com>
2016-02-21 12:04:03 -0500

    trying to make JS happen

--- public/scripts.js
+++ public/scripts.js
@@ -1,12 +1,25 @@
 $(document).ready(function() {
+
+  $("#about_text h2").html("About"); 
+  $("#about_text p").html("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt corporis molestiae dolore nisi expedita, magnam dolorum, animi, tenetur cupiditate vel exercitationem. Fugit dignissimos unde, rem optio ab, eius sint odit.");
+
   $("#history_button").click(function(){
-  $("#history_text").removeAttr("id");
-  })
+    $("#about_text h2").html("");
+    $("#about_text p").html("");
+    $("#history_text h2").html("History")
+    $("#history_text p").html("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla fugit quis sint ullam quos quia molestiae iste, animi itaque fuga, placeat nostrum temporibus laborum rem iure voluptatem ex ad dolore!");
+  });
+
+  $("#about_button").click(function(){
+    $("#about_text h2").html("About"); 
+    $("#about_text p").html("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt corporis molestiae dolore nisi expedita, magnam dolorum, animi, tenetur cupiditate vel exercitationem. Fugit dignissimos unde, rem optio ab, eius sint odit.");
+    $("#history_text h2").html("")
+    $("#history_text p").html("")
+  });
+});
   
 
 
 
 
-
-}); // end doc ready
 
