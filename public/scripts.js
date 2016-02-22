0f4f7d1bcc8603c238221a52c34cf6668884faaa
Sam Lubin <samlubin@yahoo.com>
2016-02-22 00:37:23 -0500

    turned off jquery in the about page

--- public/scripts.js
+++ public/scripts.js
@@ -1,28 +1,28 @@
-$(document).ready(function() {
+// $(document).ready(function() {
 
-  $("#about_text h2").html("About"); 
-  $("#about_text p").html("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt corporis molestiae dolore nisi expedita, magnam dolorum, animi, tenetur cupiditate vel exercitationem. Fugit dignissimos unde, rem optio ab, eius sint odit.");
+//   $("#about_text h2").html("About"); 
+//   $("#about_text p").html("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt corporis molestiae dolore nisi expedita, magnam dolorum, animi, tenetur cupiditate vel exercitationem. Fugit dignissimos unde, rem optio ab, eius sint odit.");
 
-  $("#history_button").click(function(){
-    $("#about_text h2").html("");
-    $("#about_text p").html("");
-    $("#history_text h2").html("History")
-    $("#history_text p").html("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla fugit quis sint ullam quos quia molestiae iste, animi itaque fuga, placeat nostrum temporibus laborum rem iure voluptatem ex ad dolore!");
-    return false;
-  });
+//   $("#history_button").click(function(){
+//     $("#about_text h2").html("");
+//     $("#about_text p").html("");
+//     $("#history_text h2").html("History")
+//     $("#history_text p").html("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla fugit quis sint ullam quos quia molestiae iste, animi itaque fuga, placeat nostrum temporibus laborum rem iure voluptatem ex ad dolore!");
+//     return false;
+//   });
 
-  $("#about_button").click(function(){
-    $("#about_text h2").html("About"); 
-    $("#about_text p").html("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt corporis molestiae dolore nisi expedita, magnam dolorum, animi, tenetur cupiditate vel exercitationem. Fugit dignissimos unde, rem optio ab, eius sint odit.");
-    $("#history_text h2").html("")
-    $("#history_text p").html("")
-    return false;
-  });
-
-
+//   $("#about_button").click(function(){
+//     $("#about_text h2").html("About"); 
+//     $("#about_text p").html("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt corporis molestiae dolore nisi expedita, magnam dolorum, animi, tenetur cupiditate vel exercitationem. Fugit dignissimos unde, rem optio ab, eius sint odit.");
+//     $("#history_text h2").html("")
+//     $("#history_text p").html("")
+//     return false;
+//   });
 
 
-});
+
+
+// });
   
 
 
